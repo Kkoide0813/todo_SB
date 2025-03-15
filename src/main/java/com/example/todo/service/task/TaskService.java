2c6de3695d04354945460b3fe6cdacdd44465574
Kkoide0813 <qwejks0201@gmail.com>
2025-03-16 00:37:04 +0900

    TaskService　findメソッド作成 2つのタスクをList化し、TaskControllerへreturn

---
+++ src/main/java/com/example/todo/service/task/TaskService.java
@@ -0,0 +1,28 @@
+package com.example.todo.service.task;
+
+import com.example.todo.controller.task.TaskDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+
+    public List<TaskDTO> find() {
+        var task1 = new TaskDTO(
+                1L,
+                "Spring Boot を学ぶ",
+                "TODO アプリケーションを作ってみる",
+                "TODO"
+        );
+        var task2 = new TaskDTO(
+                2L,
+                "Spring Security を学ぶ",
+                "ログイン機能を作ってみる",
+                "TODO"
+        );
+
+        return List.of(task1,task2);
+    }
+
+}

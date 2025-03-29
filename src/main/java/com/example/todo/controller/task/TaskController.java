a871a2916d9e60131c88d27699ba7691952c93de
Kkoide0813 <qwejks0201@gmail.com>
2025-03-29 13:13:47 +0900

    M-34　72. ブラウザから入力データをサーバーへ送信、データをサーバー側で受け取れるようにする

--- src/main/java/com/example/todo/controller/task/TaskController.java
+++ src/main/java/com/example/todo/controller/task/TaskController.java
@@ -43,7 +43,7 @@ public class TaskController {
 
     // POST/tasks
     @PostMapping("/tasks")
-    public String create(Model model){
+    public String create(TaskForm form, Model model){
         return list(model);
     }
 

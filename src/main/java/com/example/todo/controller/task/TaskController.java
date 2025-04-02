4c5df2c5e22c6fbbf243ac7e9b60561737b941cf
Kkoide0813 <qwejks0201@gmail.com>
2025-04-03 01:27:33 +0900

    M-56　94.編集のリクエストを受けるハンドラーメソッドを作成

--- src/main/java/com/example/todo/controller/task/TaskController.java
+++ src/main/java/com/example/todo/controller/task/TaskController.java
@@ -69,4 +69,9 @@ public class TaskController {
         model.addAttribute("mode", "EDIT"); // <title th:text="${mode == 'CREATE'} ? 'タスク作成' : 'タスク編集' "></title>
         return "tasks/form";
     }
+
+    @PutMapping("{id}") // PUT/tasks/{id}
+    public String update(@PathVariable("id") long id) {
+        return "redirect:/tasks/{id}";
+    }
 }
